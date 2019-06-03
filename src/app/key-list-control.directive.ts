da470e9b8a3439d8cc1817f56a5256384adddcf0
Sharan777 <sharangouda021@gmail.com>
2019-06-03 15:03:03 +0530

    2 files modified

--- src/app/key-list-control.directive.ts
+++ src/app/key-list-control.directive.ts
@@ -7,23 +7,34 @@ export class KeyListControlDirective {
 @Output() arrowDown:EventEmitter<any> = new EventEmitter();
   constructor(private el: ElementRef) { }
   arrowkeyLocation = 0;
-  @HostListener('keydown',['$event']) onkeydown(event: KeyboardEvent) {   
-    debugger 
+  @HostListener('keydown',['$event']) onkeydown(event: KeyboardEvent) { 
+    let maxLength = event.target.dataset.arraymaxlength;
+    maxLength = maxLength-1;
      switch (event.keyCode) {      
         case 38: // this is the ascii of arrow up
-        if(this.arrowkeyLocation > 0){
-          this.arrowkeyLocation--;
-                 this.arrowDown.emit(this.arrowkeyLocation)
-                 break;
-        }else {
-          this.arrowkeyLocation = 0;
-          break;
-        }
+                if(this.arrowkeyLocation > 0){
+                  this.arrowkeyLocation--;
+                        this.arrowDown.emit(this.arrowkeyLocation)
+                        break;
+                }else {
+                  this.arrowkeyLocation = 0;
+                  break;
+                }
                  
         case 40: // this is the ascii of arrow down 
-                 this.arrowkeyLocation++;
+                if(this.arrowkeyLocation < maxLength){
+                  this.arrowkeyLocation++;
                  this.arrowDown.emit(this.arrowkeyLocation)
                  break;
+                }else{
+                  this.arrowkeyLocation = maxLength;
+                  break;
+                }
+        case 13:
+              this.arrowkeyLocation=0;
+              this.arrowDown.emit(this.arrowkeyLocation);
+                break
+                 
     }
   }
 
